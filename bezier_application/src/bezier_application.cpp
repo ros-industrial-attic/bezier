7eee5d9215fcbfc562385b6a1af16f09f5878221
Kévin Bolloré <kevin.bollore@hotmail.fr>
2016-02-18 11:26:11 +0100

    Remove code about fsw_table and fix table, remove ply files

--- bezier_application/src/bezier_application.cpp
+++ bezier_application/src/bezier_application.cpp
@@ -64,14 +64,12 @@ int main(int argc, char **argv)
 
   // Create publishers for point clouds and markers
   ros::Publisher trajectory_publisher, input_mesh_publisher, defect_mesh_publisher, dilated_mesh_publisher,
-                 normal_publisher, fix_table_mesh_publisher, fsw_table_mesh_publisher;
+                 normal_publisher;
   trajectory_publisher    = node.advertise<visualization_msgs::Marker>("my_trajectory", 1);
   input_mesh_publisher    = node.advertise<visualization_msgs::Marker>("my_input_mesh", 1);
   defect_mesh_publisher  = node.advertise<visualization_msgs::Marker>("my_defect_mesh", 1);
   dilated_mesh_publisher  = node.advertise<visualization_msgs::Marker>("my_dilated_mesh", 1);
   normal_publisher        = node.advertise<visualization_msgs::MarkerArray>("my_normals", 1);
-  fix_table_mesh_publisher = node.advertise<visualization_msgs::Marker>("my_fix_table", 1);
-  fsw_table_mesh_publisher = node.advertise<visualization_msgs::Marker>("my_fsw_table", 1);
 
   // Generate trajectory
   double covering_percentage = 0.25; //value between 0.0 & 1.0
@@ -86,8 +84,6 @@ int main(int argc, char **argv)
   std::vector<int> index_vector;
 
   // Display in RVIZ
-  bezier_planner.displayMesh(fix_table_mesh_publisher, mesh_ressource + "environment/TableFix.ply", .1, .4, .1);
-  bezier_planner.displayMesh(fsw_table_mesh_publisher, mesh_ressource + "environment/TableFSW.ply");
   bezier_planner.displayMesh(input_mesh_publisher, mesh_ressource + input_mesh_filename);
   bezier_planner.displayMesh(defect_mesh_publisher, mesh_ressource + defect_mesh_filename, 0.6, 0.6, 0.6, 0.5);
   bezier_planner.generateTrajectory(way_points_vector, points_color_viz, index_vector);
